0a6631b771cbb04b8dfabd828f7b7516cfa81f95
bmeng5o6 <brianmeng6@gmail.com>
2026-08-13 11:42:56 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { DM_Sans, Playfair_Display, Manrope } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 
 import "./globals.css";
 
